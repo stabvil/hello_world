f400b28afdf953152c48a8d52bac94a1d63406e3
stabvil <stabvil@gmail.com>
2016-12-10 15:37:33 +0300

    del hello.txt; update hello.go

--- hello.go
+++ hello.go
@@ -4,12 +4,10 @@ import "fmt"
 
 func main() {
 	
-	for i:=1; i <= 10; i++ {
-		fmt.Print(i , " ")
-		if i % 2 == 0 {
-			fmt.Println("even")
-		} else {
-			fmt.Println("odd")
-		}
+	for i:=1; i <= 100; i++ {
+		
+		if i % 3 == 0 {
+			fmt.Println(i, "Fizz")
+		} 
 	}
 }
