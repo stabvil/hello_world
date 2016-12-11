9dae59a9679ce2592b6d24b20ed00866ecc63c6a
stabvil <stabvil@gmail.com>
2016-12-11 15:20:20 +0300

    md hello.go

--- hello.go
+++ hello.go
@@ -1,13 +1,42 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+)
 
+func pinger(c chan string) {
+    for i := 0; ; i++ {
+        c <- "ping"
+    }
+}
+func ponger(c chan string) {
+    for i := 0; ; i++ {
+        c <- "pong"
+    }
+}
+func printer1(c chan string) {
+    for {
+        msg := <- c
+        fmt.Println(msg,"1")
+        time.Sleep(time.Second * 1)
+    }
+}
+func printer2(c chan string) {
+    for {
+        msg := <- c
+        fmt.Println(msg,"2")
+        time.Sleep(time.Second * 1)
+    }
+}
 func main() {
-	
-	for i:=1; i <= 100; i++ {
-		
-		if i % 3 == 0 {
-			fmt.Println(i, "Fizz")
-		} 
-	}
+    var c chan string = make(chan string)
+
+    go pinger(c)
+	go ponger(c)
+    go printer1(c)
+	go printer2(c)
+
+    var input string
+    fmt.Scanln(&input)
 }
